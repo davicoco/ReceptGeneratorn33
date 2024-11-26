739d23a4e2b44187ce56b2350ae1a12c9738a5d1
David Izquierdo Puentes <david_izqpue@hotmail.com>
2024-11-26 20:14:56 +0100

    started method createigredient

--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class RecipeManager {
     private ArrayList<RecipeType> recipes;
 
-    public RecipeManager(){
+    public RecipeManager() {
         this.recipes = new ArrayList<>();
     }
 
@@ -17,7 +18,7 @@ public class RecipeManager {
     }
 
     public void viewRecipes() {
-        for(RecipeType r: this.recipes){
+        for (RecipeType r : this.recipes) {
             System.out.println(r.getName());
         }
     }
@@ -28,12 +29,25 @@ public class RecipeManager {
 
 
     @TODO(description = "Skriva koden till metoden createIngredient() för att skapa en lista av ingredienser")
-    public ArrayList<Ingredient> createIngredient(){
-        return null;
+    public ArrayList<Ingredient> createIngredient() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+
+        while (true) {
+            System.out.println("1. Lägg till ingrediens");
+            System.out.println("0. Färdig! Gå till nästa steg");
+            int choice = input.nextInt();
+            if(choice==1){
+                System.out.println("Vilken ingrediens vill du lägga till?");
+
+            }
+            return null;
+        }
+
     }
 
     @TODO(description = "Skriva koden till metoden createInstruction()")
-    public HashMap<Integer,String> createInstruction(){
+    public HashMap<Integer, String> createInstruction() {
         return null;
     }
 
